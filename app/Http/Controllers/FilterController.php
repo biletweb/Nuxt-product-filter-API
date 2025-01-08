08adfc020a91c6fecd296e5cdebe62d84723eb15
biletweb-macmini <bilet.igor@gmail.com>
2025-01-09 01:28:57 +0200

    fix

--- app/Http/Controllers/FilterController.php
+++ app/Http/Controllers/FilterController.php
@@ -8,6 +8,44 @@ use Illuminate\Http\Request;
 
 class FilterController extends Controller
 {
+    // public function filter(Request $request, $slug)
+    // {
+    //     $filters = $request->input('filters', []);
+    //     $offset = $request->input('offset', 0);
+    //     $limit = $request->input('limit', 10);
+    //     $category = Category::where('slug', $slug)->first();
+
+    //     if (! $category) {
+    //         return response()->json([
+    //             'error' => 'Category not found.',
+    //         ]);
+    //     }
+
+    //     $query = Product::query()
+    //         ->with('attributes.values')
+    //         ->select('id', 'name', 'price', 'sale_price')
+    //         ->where('category_id', $category->id);
+
+    //     foreach ($filters as $attribute => $valueIds) {
+    //         if (empty($valueIds)) {
+    //             continue;
+    //         }
+    //         $query->whereHas('attributes', function ($query) use ($attribute, $valueIds) {
+    //             $query->where('attribute_id', $attribute);
+    //             if (is_array($valueIds)) {
+    //                 $query->whereIn('value_id', $valueIds);
+    //             } else {
+    //                 $query->where('value_id', $valueIds);
+    //             }
+    //         });
+    //     }
+
+    //     $filteredProducts = $query->skip($offset)->take($limit)->get();
+
+    //     return response()->json([
+    //         'products' => $filteredProducts,
+    //     ]);
+    // }
     public function filter(Request $request, $slug)
     {
         $filters = $request->input('filters', []);
@@ -15,7 +53,7 @@ class FilterController extends Controller
         $limit = $request->input('limit', 10);
         $category = Category::where('slug', $slug)->first();
 
-        if (! $category) {
+        if (!$category) {
             return response()->json([
                 'error' => 'Category not found.',
             ]);
@@ -26,17 +64,18 @@ class FilterController extends Controller
             ->select('id', 'name', 'price', 'sale_price')
             ->where('category_id', $category->id);
 
-        foreach ($filters as $attribute => $valueIds) {
-            if (empty($valueIds)) {
+        foreach ($filters as $attribute => $values) {
+            if (empty($values)) {
                 continue;
             }
-            $query->whereHas('attributes', function ($query) use ($attribute, $valueIds) {
-                $query->where('attribute_id', $attribute);
-                if (is_array($valueIds)) {
-                    $query->whereIn('value_id', $valueIds);
-                } else {
-                    $query->where('value_id', $valueIds);
-                }
+
+            $query->whereHas('attributes', function ($query) use ($attribute, $values) {
+                $query->where('attributes.name', $attribute);  // фильтрация по названию атрибута
+
+                $query->whereHas('values', function ($query) use ($values) {
+                    // Преобразуем массив значений в нужный формат (по названию фильтруем)
+                    $query->whereIn('value', $values);
+                });
             });
         }
 
