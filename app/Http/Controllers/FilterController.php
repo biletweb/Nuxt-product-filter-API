80f01728a657f3044aed37061c07fc0e2b9d8a9e
biletweb-macmini <bilet.igor@gmail.com>
2025-01-14 23:13:12 +0200

    fix

--- app/Http/Controllers/FilterController.php
+++ app/Http/Controllers/FilterController.php
@@ -11,8 +11,8 @@ class FilterController extends Controller
     public function filter(Request $request, $slug)
     {
         $filters = $request->input('filters', []);
-        $offset = $request->input('offset', 0);
-        $limit = $request->input('limit', 10);
+        $offset = max(0, (int) $request->input('offset', 0));
+        $limit = max(1, (int) $request->input('limit', 10));
         $category = Category::where('slug', $slug)->first();
 
         if (! $category) {
@@ -27,14 +27,12 @@ class FilterController extends Controller
             ->where('category_id', $category->id);
 
         foreach ($filters as $attribute => $values) {
-            if (empty($values)) {
+            if (! is_array($values) || empty($values)) {
                 continue;
             }
 
             $query->whereHas('attributes', function ($query) use ($attribute, $values) {
-                $query->where('attributes.name', $attribute);
-
-                $query->whereHas('values', function ($query) use ($values) {
+                $query->where('attributes.name', $attribute)->whereHas('values', function ($query) use ($values) {
                     $query->whereIn('value', $values);
                 });
             });
